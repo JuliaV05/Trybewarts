717c3ede33883b4bf25d4b4a4e4efd7c6233ff0f
Gabriela Muniz <gabrielanamu@live.com>
2022-09-16 11:42:01 -0300

    muda estilo

---
+++ script.js
@@ -0,0 +1,25 @@
+const gitnoria = document.getElementById('button-h1');
+const reactpuff = document.getElementById('button-h2');
+const corvinode = document.getElementById('button-h3');
+const pytherina = document.getElementById('button-h4');
+const header = document.getElementsByClassName('header');
+
+gitnoria.addEventListener('click', () => {
+  document.body.style.backgroundColor = 'rgba(255, 0, 0, 0.157)';
+  header[0].style.backgroundColor = 'red';
+});
+
+reactpuff.addEventListener('click', () => {
+  document.body.style.backgroundColor = 'rgba(255, 255, 0, 0.122)';
+  header[0].style.backgroundColor = 'rgb(226, 226, 15)';
+});
+
+corvinode.addEventListener('click', () => {
+  document.body.style.backgroundColor = 'rgba(0, 0, 255, 0.188)';
+  header[0].style.backgroundColor = 'blue';
+});
+
+pytherina.addEventListener('click', () => {
+  document.body.style.backgroundColor = 'rgba(0, 128, 0, 0.212)';
+  header[0].style.backgroundColor = 'green';
+});
